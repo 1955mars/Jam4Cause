e71d5e2e048cd49beaad35733d35cc4ffa471361
Mansoor Patan <1955mars@gmail.com>
2019-11-16 13:41:53 -0500

    Player controller script

--- Jam4Cause/Assets/Scripts/Player.cs
+++ Jam4Cause/Assets/Scripts/Player.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Vector2 speedVector = new Vector2(10.0f, 0.0f);
-    public Vector2 jumpVector = new Vector2(0.0f, 20.0f);
-    public float maxSpeed = 5.0f;
-    public float maxJumpTime = 0.5f;
-    public float groundCheckDistance = 0.1f;
+
+    public float jumpForce = 5;
+    public float moveMultiplier = 5f;
+    public float maxJumpTime = 0.3f;
+    public float groundCheckDistance = 0.2f;
     public LayerMask ground;
 
     private Rigidbody2D body;
     private Transform groundChecker;
     private bool isGrounded;
     private bool jumping = false;
+
+    private float jumpTimeCounter = 0f;
+    private bool isCollidinginAir = false;
 
     // Start is called before the first frame update
     void Start()
@@ -27,34 +30,89 @@ public class Player : MonoBehaviour
     void Update()
     {
         if (Physics2D.OverlapCircle(groundChecker.position, groundCheckDistance, ground) != null)
+        {
             isGrounded = true;
+            isCollidinginAir = false;
+        }
         else
+        {
             isGrounded = false;
-    }
+        }
 
-    private void FixedUpdate()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && !jumping && isGrounded)
+        if(isGrounded && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)))
         {
             jumping = true;
-            StartCoroutine(Jump());
+            jumpTimeCounter = maxJumpTime;
+            body.velocity = Vector2.up * jumpForce;
+        }
+
+        if((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) && jumping == true)
+        {
+            if(jumpTimeCounter > 0)
+            {
+                body.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                jumping = false;
+            }
+        }
+
+        if(Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W))
+        {
+            jumping = false;
+        }
+
+
+        //if (Input.GetKeyDown(KeyCode.Space) && !jumping && isGrounded)
+        //{
+        //    jumping = true;
+        //    StartCoroutine(Jump());
+        //}
+    }
+
+    void FixedUpdate()
+    {
+        if (!isCollidinginAir)
+        {
+            body.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveMultiplier, body.velocity.y);
         }
     }
 
-    IEnumerator Jump()
+    //IEnumerator Jump()
+    //{
+    //    body.velocity = Vector2.zero;
+    //    float timer = 0.0f;
+
+    //    while (Input.GetKey(KeyCode.Space) && timer < maxJumpTime)
+    //    {
+    //        float jumpProgress = timer / maxJumpTime;
+    //        Vector2 currentJumpVector = Vector2.Lerp(jumpVector, Vector2.zero, jumpProgress);
+    //        body.AddForce(currentJumpVector);
+    //        timer += Time.deltaTime;
+    //        yield return null;
+    //    }
+
+    //    jumping = false;
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        body.velocity = Vector2.zero;
-        float timer = 0.0f;
-
-        while (Input.GetKey(KeyCode.Space) && timer < maxJumpTime)
+        if (jumping)
         {
-            float jumpProgress = timer / maxJumpTime;
-            Vector2 currentJumpVector = Vector2.Lerp(jumpVector, Vector2.zero, jumpProgress);
-            body.AddForce(currentJumpVector);
-            timer += Time.deltaTime;
-            yield return null;
+            Debug.Log("Jumping and colliding");
+            isCollidinginAir = true;
         }
-
-        jumping = false;
+            
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (jumping && isCollidinginAir)
+        {
+            Debug.Log("Jumping and non-colliding");
+            isCollidinginAir = false;
+        }
+            
     }
 }
---
+++ Jam4Cause/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector2 velocity;
+    float force = 0.05f;
+    float moveMultiplier = 5f;
+    float jumpHeight = 5f;
+
+    Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        else
+        {
+            velocity.x = Input.GetAxis("Horizontal");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = new Vector2(velocity.x * moveMultiplier, rigidbody.velocity.y);
+        //rigidbody.velocity = Vector2.zero;
+    }
+
+    void Jump()
+    {
+        velocity.y = Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics2D.gravity.y));
+        rigidbody.velocity = new Vector2(velocity.x, velocity.y);
+        velocity.y += Physics2D.gravity.y * Time.deltaTime;
+        rigidbody.velocity = new Vector2(velocity.x, velocity.y);
+    }
+}
