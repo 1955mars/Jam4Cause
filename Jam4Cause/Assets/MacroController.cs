3cf58dc56e8ee3f487a7e7b0bf1fa41c6e0793d1
Robert MacLeod (RIT Student) <rm6838@ad.rit.edu>
2019-11-15 23:44:37 -0500

    New Tiles
    Added complete tileset

---
+++ Jam4Cause/Assets/MacroController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+struct FrameData
+{
+    public bool jumpPressed;
+    public bool rightPressed;
+    public bool leftPressed;
+    public bool actionPressed;
+}
+
+public class MacroController : MonoBehaviour
+{
+    private List<FrameData> macro1;
+    private List<FrameData> macro2;
+    private List<FrameData> macro3;
+
+    [SerializeField]
+    private bool recording;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        recording = false;
+        macro1 = new List<FrameData>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            recording = !recording;
+        }
+
+        if (recording)
+        {
+            FrameData currentFrame = new FrameData();
+
+            currentFrame.jumpDown = Input.GetKeyDown(KeyCode.Space);
+            currentFrame.rightPressed = Input.GetKeyDown(KeyCode.Space);
+
+            macro1.Add(currentFrame);
+        }
+    }
+}
