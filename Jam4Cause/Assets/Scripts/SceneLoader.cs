cb20b98e0764c7a5ecd7163532f2fd654d6f9993
DarkPhantomEx <54686142+DarkPhantomEx@users.noreply.github.com>
2019-11-16 15:16:08 -0500

    UI update

---
+++ Jam4Cause/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void Scene_loader(int scene_index)
+    {
+        SceneManager.LoadScene(scene_index);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
