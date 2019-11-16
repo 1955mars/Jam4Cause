9bbb9df5c492654c9d914b90a81836858b30b43b
Mansoor Patan <1955mars@gmail.com>
2019-11-16 13:41:53 -0500

    Player controller script

---
+++ Jam4Cause/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform lookAtObject;
+
+    float offsetX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offsetX = transform.position.x - lookAtObject.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newX = lookAtObject.transform.position.x;
+        transform.position = new Vector3(offsetX + newX, transform.position.y, transform.position.z);
+    }
+}
