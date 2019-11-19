6940b4a4ddb391c20a794fdfadd4bd1f8d46732e
Richard Hetley (RIT Student) <rsh5353@ad.rit.edu>
2019-11-19 12:15:36 -0500

    Death spike sprites and remaining exit doors
    If the student runs into a spike, the level resets.  If the player runs into a spike, the existing tile in the map (which is still there, behind the otherwise-identical sprite) stop the player's movement.  The player is nonetheless invincible

---
+++ Jam4Cause/Assets/Scripts/DeathOrRevert.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class DeathOrRevert : MonoBehaviour
+{
+    int numberOfLevels = 5; // The menu is scene 0
+    public static int currentLevel = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Student")
+        {
+            // Just go back to the start of this level
+            SceneManager.LoadScene(currentLevel);
+            Debug.Log("Restarting on death");
+        }
+        else if (other.gameObject.tag == "Player")
+        {
+            // This would end the current macro if possible
+        }
+    }
+}
